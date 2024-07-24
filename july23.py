7da8d1c956a87ad5517b14bc41bcd863a5f2b1dd
vishnu <111786064+vishnubalai070@users.noreply.github.com>
2024-07-24 12:42:49 +0530

    Add files via upload

---
+++ july23.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[21]:
+
+
+"logical operators"
+
+n=25
+print(n<20 or n<30)
+
+
+# In[20]:
+
+
+n=24
+print(n>23 and n>30)
+
+
+# In[29]:
+
+
+a=input("enter a number")
+print(a, not 30)
+
+
+# In[31]:
+
+
+"input function"
+a=("vishnu")
+b=10
+c=3.4
+d=[3,4,5]
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d)) 
+
+
+# In[1]:
+
+
+"convertion int float str "
+a=str(input('enter a number'))
+print(a)
+print(type(a))
+
+
+# In[2]:
+
+
+"convertion int float str "
+a=float(input('enter a number'))
+print(a)
+print(type(a))
+      
+
+
+# In[18]:
+
+
+"sep and end arugument"
+str1="code"
+str2="io"
+print(str1,'_', sep='', end='')
+print(str2)
+
+
+# In[ ]:
+
+
+
+
